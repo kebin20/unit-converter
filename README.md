8098c6cf8501bc99265ebde9fc3c1026dbb9f926
kebin20 <ktanzyl@hotmail.com>
2022-09-17 10:22:09 +0900

    Created basic file setup, with code predone on codepen

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # unit-converter
 A basic Javascript app that converts the most common units of measurements
+
+Main techniques used:
+
+-  Manipulation of the DOM 
+-  Use of  functions
+- Use of template literals
---
+++ app.js
@@ -0,0 +1,50 @@
+// HTML Components
+const unitInput = document.querySelector("#unit-input");
+const convertBtn = document.querySelector(".convert-btn");
+const mTf = document.querySelector("#mtf");
+const fTm = document.querySelector("#ftm");
+const lTg = document.querySelector("#ltg");
+const gTl = document.querySelector("#gtl");
+const kTp = document.querySelector("#ktp");
+const pTk = document.querySelector("#ptk");
+
+convertBtn.addEventListener("click", () => {
+  let unit = parseInt(unitInput.value);
+  meterFeet(unit);
+  feetMeter(unit);
+  literGallon(unit);
+  gallonLiter(unit);
+  kiloPound(unit);
+  poundKilo(unit);
+});
+
+// Formulas
+function meterFeet(unit) {
+  let feet = (unit * 3.28084).toFixed(3);
+  mTf.textContent = `${unit} meters = ${feet} feet  |`;
+}
+
+function feetMeter(unit) {
+  let meter = (unit * 0.3048).toFixed(3);
+  fTm.textContent = `  ${unit} feet = ${meter} meters`;
+}
+
+function literGallon(unit) {
+  let gallon = (unit * 0.264172).toFixed(3);
+  lTg.textContent = `${unit} liters = ${gallon} gallons  |`;
+}
+
+function gallonLiter(unit) {
+  let liter = (unit * 3.785412).toFixed(3);
+  gTl.textContent = `  ${unit} gallons = ${liter} liters`;
+}
+
+function kiloPound(unit) {
+  let pound = (unit * 2.2).toFixed(3);
+  kTp.textContent = `${unit} kilograms = ${pound} pounds  |`;
+}
+
+function poundKilo(unit) {
+  let kilo = (unit * 0.45359237).toFixed(3);
+  pTk.textContent = ` ${unit} pounds = ${kilo} kilograms`;
+}
