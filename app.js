9e60759dbf8cc2a7477c2b0c3c3b30e9cfca2075
kebin20 <ktanzyl@hotmail.com>
2022-11-02 22:18:38 +0900

    Cleaned up code

--- app.js
+++ app.js
@@ -9,43 +9,42 @@ const kTp = document.querySelector("#ktp");
 const pTk = document.querySelector("#ptk");
 
 convertBtn.addEventListener("click", () => {
-  let unit = parseInt(unitInput.value)
-  meterFeet(unit)
-  feetMeter(unit)
-  literGallon(unit)
-  gallonLiter(unit)
-  kiloPound(unit)
-  poundKilo(unit)
-})
-
+  const unit = parseInt(unitInput.value);
+  meterFeet(unit);
+  feetMeter(unit);
+  literGallon(unit);
+  gallonLiter(unit);
+  kiloPound(unit);
+  poundKilo(unit);
+});
 
 // Formulas
 function meterFeet(unit) {
-  let feet = (unit*3.28084).toFixed(3)
-  mTf.textContent = `${unit} meters = ${feet} feet  |`
+  const feet = (unit * 3.28084).toFixed(3);
+  mTf.textContent = `${unit} meters = ${feet} feet  |`;
 }
 
 function feetMeter(unit) {
-  let meter = (unit*0.3048).toFixed(3)
-  fTm.textContent = `  ${unit} feet = ${meter} meters`
+  const meter = (unit * 0.3048).toFixed(3);
+  fTm.textContent = `  ${unit} feet = ${meter} meters`;
 }
 
 function literGallon(unit) {
-  let gallon = (unit*0.264172).toFixed(3)
-  lTg.textContent = `${unit} liters = ${gallon} gallons  |`
+  const gallon = (unit * 0.264172).toFixed(3);
+  lTg.textContent = `${unit} liters = ${gallon} gallons  |`;
 }
 
 function gallonLiter(unit) {
-  let liter = (unit*3.785412).toFixed(3)
-  gTl.textContent = `  ${unit} gallons = ${liter} liters`
+  const liter = (unit * 3.785412).toFixed(3);
+  gTl.textContent = `  ${unit} gallons = ${liter} liters`;
 }
 
 function kiloPound(unit) {
-  let pound = (unit*2.2).toFixed(3)
-  kTp.textContent = `${unit} kilograms = ${pound} pounds  |`
+  const pound = (unit * 2.2).toFixed(3);
+  kTp.textContent = `${unit} kilograms = ${pound} pounds  |`;
 }
 
 function poundKilo(unit) {
-  let kilo = (unit*0.45359237).toFixed(3)
-  pTk.textContent = ` ${unit} pounds = ${kilo} kilograms`
+  const kilo = (unit * 0.45359237).toFixed(3);
+  pTk.textContent = ` ${unit} pounds = ${kilo} kilograms`;
 }
